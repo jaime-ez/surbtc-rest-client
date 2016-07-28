efc012a3e81d0df73617878d806995a4ac253494
jaime-e <jechegarayz@gmail.com>
2016-07-28 10:39:33 -0400

    [test-fix] check for all markets

--- test/get_order_book.js
+++ test/get_order_book.js
@@ -4,33 +4,36 @@ var Client = require('../')
 var assert = require('chai').assert
 var errorFixture = require('./fixtures/get_order_book').error
 var success = require('./fixtures/get_order_book').success
-var marketId = require('./fixtures/get_order_book').markets()[0]
-
-/*
-  How can i test all markets at once? With async or forEach I have callback problems
-*/
+var markets = require('./fixtures/get_order_book').markets()
+var async = require('async')
 
 describe('Surbtc REST Client Get Order Book', function () {
-  it('should get order books for market ' + marketId, function (done) {
-    var client = new Client({})
-    client.getOrderBook(marketId, function (error, response) {
-      assert(!error)
-      assert(response)
-      assert.deepEqual(success(response), response)
-      done()
+  async.eachSeries(markets, function (marketId, cb) {
+    it('should get order books for market ' + marketId, function (done) {
+      var client = new Client({})
+      client.getOrderBook(marketId, function (error, response) {
+        assert(!error)
+        assert(response)
+        assert.deepEqual(success(response), response)
+        done()
+      })
     })
+    cb()
   })
 
-  it('should fail to get order books for market ' + marketId, function (done) {
-    var client = new Client({
-      api: 'https://surbtc.com/api/v1/marketmarket'
-    })
+  async.eachSeries(markets, function (marketId, cb) {
+    it('should fail to get order books for market ' + marketId, function (done) {
+      var client = new Client({
+        api: 'https://surbtc.com/api/v1/marketmarket'
+      })
 
-    client.getOrderBook(marketId, function (error, response) {
-      assert(error)
-      assert(!response)
-      assert.deepEqual(errorFixture(), error)
-      done()
+      client.getOrderBook(marketId, function (error, response) {
+        assert(error)
+        assert(!response)
+        assert.deepEqual(errorFixture(), error)
+        done()
+      })
     })
+    cb()
   })
 })
